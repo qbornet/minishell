f749efb3a2c414159f31f0cc85aabb9843368ba2
Quentin BORNET <qbornet@e2r10p1.clusters.42paris.fr>
2022-03-31 17:53:49 +0200

    change ft_pwd to int return

--- pwd.c
+++ pwd.c
@@ -1,12 +1,10 @@
 #include "bin.h"
 
-void	ft_pwd(void)
+int	ft_pwd(void)
 {
 	char	*cwd;
 
 	cwd = getcwd(NULL, PATH_MAX);
 	if (cwd)
-		printf("%s\n", cwd);
-	else
-		printf("NULL\n");
+		return (printf("%s\n", cwd));
 }
