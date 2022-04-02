e8b02cc514864dcda78fb2bc7ac2a367abc5d779
Quentin Bornet <ring0.csgo@gmail.com>
2022-04-02 05:20:29 +0200

    WIP, for know just endl function and history added, need to check further in the doc cuz right know readline is not really clear to me

--- prompt.h
+++ prompt.h
@@ -3,7 +3,6 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-
-
+# define PROMPT "$> "
 
 #endif
--- start_prompt.c
+++ start_prompt.c
@@ -1,6 +1,35 @@
 #include "prompt.h"
 
+/* ft_endl creer une nouvelle ligne pour readline
+ * apres chaque commande entree*/
+
+void	ft_endl(void)
+{
+	rl_replace_line("\n", 0);
+	rl_redisplay();
+	rl_redisplay();
+	rl_on_new_line();
+}
+
+void	ft_history(char *str)
+{
+	if (str && *str)
+		add_history(str);
+}
+
+/* temp existe juste pour rentrer dans le while, oui oui c'est overkill */
+
 int	main(void)
 {
+	char	temp;
+	char	*str;
+
+	str = &temp;
+	while (str)
+	{
+		str = readline(PROMPT);
+		ft_history(str);
+		ft_endl();
+	}
 	return (0);
 }
