8676b7f61bcd04bec6d2bbc8670f32f7f00a965d
jfrancai <julienfrancais@outlook.fr>
2022-05-12 09:29:43 +0200

    $ expansion OK with vars in env

--- srcs/varexp.c
+++ srcs/varexp.c
@@ -2,7 +2,7 @@
 
 int	ft_varlen(char *s)
 {
-	char *tmp;
+	char	*tmp;
 
 	if (!s)
 		return (0);
@@ -21,12 +21,45 @@ char	*check_var_env(char **env, char *var_name)
 	return (*env);
 }
 
+void	new_data(char *tmp, int len, t_strlist **strlst, char *result)
+{
+	int	size;
+
+	size = len - ft_strlen(tmp);
+	while (len--)
+	{
+		if (len >= size)
+			result[len] = tmp[len - size];
+		else
+			result[len] = ((char *)(*strlst)->data)[len];
+	}
+}
+
+char	*expand_var(char **s, t_strlist **strlst, char **env)
+{
+	int		len;
+	char	*tmp;
+	char	*result;
+
+	result = NULL;
+	tmp = check_var_env(env, *s + 1);
+	if (tmp)
+	{
+		while (*tmp && *tmp != '=')
+			tmp++;
+		tmp++;
+		len = *s - (char *)(*strlst)->data + ft_strlen(tmp);
+		result = ft_calloc(len + 1, sizeof(char));
+		if (!result)
+			return (NULL);
+		new_data(tmp, len, strlst, result);
+	}
+	return (result);
+}
+
 void	expand(t_strlist *strlst, char **env)
 {
 	char	*s;
-	int		len;
-	char	*tmp;
-	int		tmplen;
 	char	*result;
 
 	while (strlst)
@@ -36,28 +69,9 @@ void	expand(t_strlist *strlst, char **env)
 			s++;
 		if (*s == '$')
 		{
-			tmp = check_var_env(env, s + 1);
-			if (tmp)
-			{
-				while (*tmp && *tmp != '=')
-					tmp++;
-				tmp++;
-				tmplen = ft_strlen(tmp);
-				len = s - (char *)strlst->data + tmplen;
-				tmplen = len - tmplen;
-				result = ft_calloc(len + 1, sizeof(char));
-				if (!result)
-					return ;
-				while (len--)
-				{
-					if (len >= tmplen)
-						result[len] = tmp[len - tmplen];
-					else
-						result[len] = ((char *)strlst->data)[len];
-				}
-				free(strlst->data);	
-				strlst->data = result;
-			}
+			result = expand_var(&s, &strlst, env);
+			free(strlst->data);
+			strlst->data = result;
 		}
 		strlst = strlst->next;
 	}
