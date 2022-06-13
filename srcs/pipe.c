7894bbd58fc3a55924d87fd606539abbcbe4bc88
jfrancai <julienfrancais@outlook.fr>
2022-06-13 13:22:02 +0200

    norminette ft_pipe

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -34,7 +34,6 @@ int	ft_pipe(t_cmdblock **cmdblock, char **envp)
 	int			*pids;
 	int			len_cmdb;
 	int			i;
-	t_cmdblock	*usage;
 
 	len_cmdb = ft_len_cmdblock(*cmdblock);
 	if (alloc_pipes_pids(&pipes, &pids, len_cmdb))
@@ -47,12 +46,11 @@ int	ft_pipe(t_cmdblock **cmdblock, char **envp)
 			return (free_and_msg(pipes, pids, 0, "fork"));
 		if (pids[i] == 0)
 		{
-			usage = ft_next_cmdblock(i, cmdblock);
 			if (close_pipes(pipes, len_cmdb, pids, i) == -1)
 				return (1);
 			if (open_fd(pipes, cmdblock, len_cmdb, i) == -1)
 				return (free_and_return(pipes, pids, 0, 1));
-			if (pipex(pipes, pids, envp, usage) == -1)
+			if (pipex(pipes, pids, envp, ft_next_cmdblock(i, cmdblock)) == -1)
 				return (errno);
 		}
 	}
