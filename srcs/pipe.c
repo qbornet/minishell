c7dba71aa9ce0470c9dafd6605689ad8334d2186
Quentin Bornet <ring0.csgo@gmail.com>
2022-06-13 12:51:55 +0200

    fix, pipe result, wip

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -1,5 +1,18 @@
 #include "minishell.h"
 
+static t_cmdblock	*ft_next_cmdblock(int i, t_cmdblock **curr)
+{
+	t_cmdblock	*cmdblk;
+
+	cmdblk = *curr;
+	while (i && cmdblk->next)
+	{
+		cmdblk = cmdblk->next;
+		i--;
+	}
+	return (cmdblk);
+}
+
 int	ft_len_cmdblock(t_cmdblock *cmdblock)
 {
 	int	i;
@@ -17,10 +30,11 @@ int	ft_len_cmdblock(t_cmdblock *cmdblock)
 
 int	ft_pipe(t_cmdblock **cmdblock, char **envp)
 {	
-	int		**pipes;
-	int		*pids;
-	int		len_cmdb;
-	int		i;
+	int			**pipes;
+	int			*pids;
+	int			len_cmdb;
+	int			i;
+	t_cmdblock	*usage;
 
 	len_cmdb = ft_len_cmdblock(*cmdblock);
 	if (alloc_pipes_pids(&pipes, &pids, len_cmdb))
@@ -33,11 +47,12 @@ int	ft_pipe(t_cmdblock **cmdblock, char **envp)
 			return (free_and_msg(pipes, pids, 0, "fork"));
 		if (pids[i] == 0)
 		{
+			usage = ft_next_cmdblock(i, cmdblock);
 			if (close_pipes(pipes, len_cmdb, pids, i) == -1)
 				return (1);
 			if (open_fd(pipes, cmdblock, len_cmdb, i) == -1)
 				return (free_and_return(pipes, pids, 0, 1));
-			if (pipex(pipes, pids, envp, *cmdblock) == -1)
+			if (pipex(pipes, pids, envp, usage) == -1)
 				return (errno);
 		}
 	}
