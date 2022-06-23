ac396577e7150aa5cb0eff922ca4814f494b8595
jfrancai <julienfrancais@outlook.fr>
2022-06-23 11:33:06 +0200

    update ft_strdup.c len use

--- libft/srcs/ft_strdup.c
+++ libft/srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jfrancai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 12:34:02 by jfrancai          #+#    #+#             */
-/*   Updated: 2022/06/23 11:19:44 by jfrancai         ###   ########.fr       */
+/*   Updated: 2022/06/23 11:29:34 by jfrancai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char	*ft_strdup(const char *s)
 	char	*str;
 	int		len;
 
-	len = ft_strlen(s) + 1;
-	str = (char *)ft_calloc(len, sizeof(char));
+	len = ft_strlen(s);
+	str = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	while (len--)
