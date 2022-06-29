1478f0ee82ebe8eb0ea4ffee97bef12b299637a4
Julien FRANCAIS <jfrancai@student.42.fr>
2022-06-29 08:49:46 +0200

    bug fixe on cd too many args

--- srcs/exec.c
+++ srcs/exec.c
@@ -95,6 +95,7 @@ int	run_exec(t_data **frame)
 	if (pr->len_cmdb - 1 == 0)
 	{
 		exec_code = exec_builtin_single((*frame)->cmdblk, frame);
+		printf("exec_code %d\n", exec_code);
 		if (!exec_code || exec_code < 0)
 			return (0);
 	}
