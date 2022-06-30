18b2d3ddb73c77a4cdcba3c4d5ac67a97ab7b502
Quentin Bornet <ring0.csgo@gmail.com>
2022-06-30 15:30:44 +0200

    fix, exit now exit properly when argument is wrong

--- srcs/exit.c
+++ srcs/exit.c
@@ -2,7 +2,7 @@
 
 /* :0 */
 
-static void	opt_exit(t_cmdblock *cmdblk)
+static int	opt_exit(t_cmdblock *cmdblk)
 {
 	size_t		i;
 	size_t		j;
@@ -13,27 +13,34 @@ static void	opt_exit(t_cmdblock *cmdblk)
 	{
 		str = ft_strdup(cmdblk->cmd[i]);
 		if (!str)
-			return ;
+			return (-1);
 		j = -1;
 		while (str[++j])
 		{
 			if (!ft_isdigit(str[j]))
 			{
-				ft_putstr_fd("exit: numeric argumenet required '", 2);
+				ft_putstr_fd("exit: numeric argument required '", 2);
 				ft_putstr_fd(str, 2);
 				ft_putendl_fd("'", 2);
 				free(str);
-				break ;
+				return (1);
 			}
 		}
 		free(str);
 	}
+	return (0);
 }
 
 void	ft_exit(t_data **d_curr, int status)
 {
-	opt_exit((*d_curr)->cmdblk);
+	int	ret;
+
+	ret = opt_exit((*d_curr)->cmdblk);
+	if (ret < 0)
+		exit_group(d_curr);
 	write(1, "exit\n", 5);
+	if (ret)
+		exit_group(d_curr);
 	ft_free_all(d_curr);
 	exit(status);
 }
