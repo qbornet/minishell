3285c9b9519f692eac01b006b3a6d7e57c5fce95
jfrancai <julienfrancais@outlook.fr>
2022-07-02 13:02:41 +0200

    bug fix pipex

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,13 +1,18 @@
 #include "minishell.h"
 
-int	pipex(t_process *pr, t_data **frame, t_cmdblock *cmdblock)
+int	pipex(t_data **frame, t_cmdblock *cmdblock)
 {
+	int	code;
+
 	if (!cmdblock->cmd)
 		return (-1);
 	if (g_exit_status == 130)
 		exit(g_exit_status);
-	if (is_builtin(cmdblock, frame) == 0)	
+	code = is_builtin(cmdblock, frame);
+	if (code == 0)
 		exit(0);
+	if (code < 0)
+		return (-1);
 	if (get_cmd_tab(cmdblock, (*frame)->envp) < 0)
 		return (-1);
 	if (exec_cmd(cmdblock, (*frame)->envp) < 0)
