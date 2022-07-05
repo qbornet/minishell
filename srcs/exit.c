b6f60bd0f696d1b8e5dd08ca4b5022413fe34452
Quentin Bornet <ring0.csgo@gmail.com>
2022-07-05 16:47:12 +0200

    fix, exit pass all the test, fix export to not recognize '-' as an option if a '=' is found on args

--- srcs/exit.c
+++ srcs/exit.c
@@ -2,7 +2,27 @@
 
 /* :0 */
 
-static int	opt_exit(t_cmdblock *cmdblk)
+static int	ft_isvalidargs(char *str)
+{
+	size_t	i;
+	size_t	count;
+
+	i = -1;
+	count = 0;
+	while (str[++i])
+	{
+		while (str[i] == '-' || str[i] == '+')
+		{
+			i++;
+			count++;
+		}
+		if (count > 1 || !ft_isdigit(str[i]))
+				return (0);
+	}
+	return (1);
+}
+
+static int	opt_exit(t_cmdblock *cmdblk, t_data **d_curr)
 {
 	size_t		i;
 	size_t		j;
@@ -15,16 +35,15 @@ static int	opt_exit(t_cmdblock *cmdblk)
 		if (!str)
 			return (-1);
 		j = -1;
-		while (str[++j])
+		if (!ft_isvalidargs(str)
+				|| !ft_strcmp(str, "-9223372036854775809") || !ft_strcmp(str, "9223372036854775808"))
 		{
-			if (!ft_isdigit(str[j]) || (str[j] == '-' && !ft_isdigit(str[j + 1])))
-			{
-				ft_putstr_fd("exit: numeric argument required '", 2);
-				ft_putstr_fd(str, 2);
-				ft_putendl_fd("'", 2);
-				free(str);
-				return (1);
-			}
+			ft_putstr_fd("exit: numeric argument required '", 2);
+			ft_putstr_fd(str, 2);
+			ft_putendl_fd("'", 2);
+			free(str);
+			ft_free_all(d_curr);
+			exit(2);
 		}
 		free(str);
 	}
@@ -33,18 +52,25 @@ static int	opt_exit(t_cmdblock *cmdblk)
 
 void	ft_exit(t_data **d_curr)
 {
+	int				i;
 	int				ret;
 	unsigned char	status;
 
-	status = 0;
+	status = g_exit_status;
 	write(1, "exit\n", 5);
-	ret = opt_exit((*d_curr)->cmdblk);
+	ret = opt_exit((*d_curr)->cmdblk, d_curr);
 	if (ret < 0)
 		exit_group(d_curr);
-	if (ret)
-		exit_group(d_curr);
-	if ((*d_curr)->cmdblk->cmd[2] != NULL)
-		ft_putendl_fd("minishell: exit: too many arguments", 2);
+	i = -1;
+	while ((*d_curr)->cmdblk->cmd[++i])
+	{
+		if (i > 1)
+		{
+			ft_putendl_fd("minishell: exit: too many arguments", 2);
+			ft_free_all(d_curr);
+			exit(1);
+		}
+	}
 	if ((*d_curr)->cmdblk->cmd[1])
 		status = ft_atoi((*d_curr)->cmdblk->cmd[1]);
 	ft_free_all(d_curr);
